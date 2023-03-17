17bcde9cb31e4bff38e9738dde442f3c5f9565b2
feeby2494 <toby2494@hotmail.com>
2023-03-16 22:46:55 -0500

    added "/" before index.html for the try_files directive in nginx.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import HomeView from '../views/HomeView.vue';
 import ContactView from '../views/ContactView.vue';
 import AboutView from '../views/AboutView.vue';
 
+
+import { createRouter, createWebHistory } from 'vue-router'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
